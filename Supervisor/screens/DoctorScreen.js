0697e8018305974b2dc7ba4da41bfe9c6e5584ba
Anamika Mishra <anamikam632@gmail.com>
2024-03-29 14:17:40 +0530

    all

--- Supervisor/screens/DoctorScreen.js
+++ Supervisor/screens/DoctorScreen.js
@@ -1,89 +1,193 @@
-import React from 'react';
-import { FlatList, View, Text, StyleSheet,Switch } from 'react-native';
-import Card from '../components/Card';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FlatList, View, Text, StyleSheet, Pressable, TouchableOpacity, Modal } from 'react-native';
+import { SearchBar, Icon } from 'react-native-elements';
+import DoctorCard from '../components/DoctorCard';
+import RadioButton from '../components/RadioButton';
+import AddUser from '../AddUser/AddUser';
 // Sample data
-const data = [
-  { id: 1, name: 'John', tluka: 'A' ,specialization:'xyz'},
-  { id: 2, name: 'Jane', tluka: 'B' ,specialization:'xyz'},
-  { id: 3, name: 'Bob', tluka: 'C' ,specialization:'xyz'},
-  { id: 4, name: 'John', tluka: 'A' ,specialization:'xyz'},
-  { id: 5, name: 'Jane', tluka: 'B' ,specialization:'xyz'},
-  { id: 6, name: 'Bob', tluka: 'C' ,specialization:'xyz'},
-  { id: 7, name: 'John', tluka: 'A' ,specialization:'xyz'},
-  { id: 8, name: 'Jane', tluka: 'B' ,specialization:'xyz'},
-  { id: 9, name: 'Bob', tluka: 'C' ,specialization:'xyz'},
-  { id: 10, name: 'John', tluka: 'A' ,specialization:'xyz'},
-  { id: 11, name: 'Jane', tluka: 'B' ,specialization:'xyz'},
-  { id: 12, name: 'Bob', tluka: 'C' ,specialization:'xyz'},
-  { id: 13, name: 'John', tluka: 'A' ,specialization:'xyz'},
-  { id: 14, name: 'Jane', tluka: 'B' ,specialization:'xyz'},
-  { id: 15, name: 'Bob', tluka: 'C' ,specialization:'xyz'},
-  // Add more data as needed
-];
+
 
 const TableHeader = () => (
   <View style={styles.tableRow}>
-    <Text style={[styles.tableCell, { flex: 1 },{fontWeight: 'bold'}]}>ID</Text>
-    <Text style={[styles.tableCell, { flex: 2 },{fontWeight: 'bold'}]}>Name</Text>
-    <Text style={[styles.tableCell, { flex: 1 },{fontWeight: 'bold'}]}>Taluka</Text>
-    <Text style={[styles.tableCell, { flex: 2 },{fontWeight: 'bold'}]}>Specialization</Text>
+    <Text style={[styles.tableCell, { flex: 1 }, { fontWeight: 'bold' }]}>ID</Text>
+    <Text style={[styles.tableCell, { flex: 3 }, { fontWeight: 'bold' }]}>Name</Text>
+    <Text style={[styles.tableCell, { flex: 2 }, { fontWeight: 'bold' }]}>Taluka</Text>
+    <Text style={[styles.tableCell, { flex: 2 }, { fontWeight: 'bold' }]}>Specialization</Text>
   </View>
 );
+const DoctorScreen = ({ navigation }) => {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [valueFromRadio, setValueFromRadio] = useState(1);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-const TableRow = ({ item }) => (
-  <View style={styles.tableRow}>
-    <Text style={[styles.tableCell, { flex: 1 }]}>{item.id}</Text>
-    <Text style={[styles.tableCell, { flex: 2 }]}>{item.name}</Text>
-    <Text style={[styles.tableCell, { flex: 1 }]}>{item.tluka}</Text>
-    <Text style={[styles.tableCell, { flex: 2 }]}>{item.specialization}</Text>
-  </View>
-);
+  const handleSearch = (text) => {
+    console.log(valueFromRadio);
+    const filteredSearchData = data.filter((item) => {
+      return valueFromRadio === 1 ?
+        (item.name.toLowerCase().includes(text.toLowerCase())) :
+        (item.taluka.toLowerCase().includes(text.toLowerCase()))
+    }
+    );
+    setSearchQuery(text);
+    setFilteredData(filteredSearchData);
+  };
+  const handleChildValueChange = (newValue) => {
+    console.log("newValue: ", newValue);
+    setSearchQuery("");
+    setValueFromRadio(newValue);
+  };
+  const onSelectUser = (user) => {
+    console.log("selecteduser", user);
+    setSelectedUser(user);
+  }
+  const showModal = () => {
+    console.log("Show Modal");
+    setIsModalVisible(true);
+  };
 
-const FieldWorkerScreen = () => {
+  const saveModal = () => {
+    console.log("Save Modal");
+    setIsModalVisible(false);
+  };
+
+  const TableRow = ({ item }) => {
+    console.log("item", item);
+    return (
+      <Pressable onPress={() => onSelectUser(item)}>
+        <View style={styles.tableRow}>
+          <Text style={[styles.tableCell, { flex: 1 }]}>{item.id}</Text>
+          <Text style={[styles.tableCell, { flex: 3 }]}>{item.name}</Text>
+          <Text style={[styles.tableCell, { flex: 2 }]}>{item.taluka}</Text>
+          <Text style={[styles.tableCell, { flex: 2 }]}>{item.specialization}</Text>
+        </View>
+      </Pressable>
+    )
+  };
+  useEffect(() => {
+    // Make API call on component mount
+    axios.get('http://10.0.2.2:3001/doctor')
+      .then(response => {
+        // Update state with API data
+        console.log("response", response);
+        setData(response.data);
+        setSelectedUser(response.data[0]);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-        <View style={styles.list}>
-    <FlatList 
-      data={data}
-      ListHeaderComponent={<TableHeader />}
-      renderItem={({ item }) => <TableRow item={item} />}
-      keyExtractor={item => item.id.toString()}
-      showsVerticalScrollIndicator = {false}
-    />
-    </View>
-    <View style={styles.card}>   
-    </View>
+      <View style={styles.list}>
+        <View style={{ marginTop: 20, marginBottom: 20, height: 110 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              height: 70,
+              alignItems: 'center',
+              backgroundColor: "white",
+              marginRight: 60,
+
+            }}
+          >
+            <SearchBar
+              placeholder="Search"
+              onChangeText={handleSearch}
+              value={searchQuery}
+              inputStyle={{ backgroundColor: 'white', color: 'black' }} // Style for the text input
+              containerStyle={{
+                backgroundColor: 'transparent',
+                borderBottomColor: 'transparent',
+                borderTopColor: 'transparent',
+                borderWidth: 2,
+                borderColor: 'black',
+                borderRadius: 24,
+                width: "80%",
+                margin: 20,
+                marginBottom: 0,
+              }} // Style for the container
+              inputContainerStyle={{ backgroundColor: 'white', borderRadius: 10, height: 30 }} // Style for the input container
+              searchIcon={{ size: 24 }} // Style for the search icon
+              clearIcon={{ size: 24 }} // Style for the clear icon
+            />
+            <TouchableOpacity onPress={showModal}>
+              <View style={styles.circle}>
+                <Icon name="plus" type="font-awesome" color="black" />
+              </View>
+            </TouchableOpacity>
+          </View>
+          <RadioButton onValueChange={handleChildValueChange} />
+        </View>
+        <View style={styles.flatlist}>
+          <FlatList
+            data={searchQuery ? filteredData : data}
+            ListHeaderComponent={<TableHeader />}
+            renderItem={({ item }) => <TableRow item={item} />}
+            keyExtractor={item => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+      <View style={styles.card}>
+        {selectedUser && <DoctorCard user={selectedUser} />}
+      </View>
+      {/* Modal */}
+      <Modal visible={isModalVisible} transparent animationType="slide">
+        <AddUser saveModal={saveModal}/>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container : {
-        flexDirection : 'row',
-        flex : 1,
-        justifyContent: 'safe'
-    },
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+  },
   tableRow: {
     flexDirection: 'row',
     borderColor: 'white',
-    backgroundColor:'white',
+    backgroundColor: 'white',
     paddingVertical: 10,
-    marginEnd: 10
+    marginEnd: 30,
   },
   tableCell: {
-    textAlign: 'center',
+    textAlign: 'left',
     fontSize: 18,
+    fontStyle: 'bold',
+    justifyContent: 'center',
   },
-  list : {
-    flex : 0.5,
+  flatlist: {
+    marginTop: 50,
+    flex: 2,
+    backgroundColor: 'white',
+    marginRight: 20,
+    marginLeft:20
+  },
+  list: {
+    flex: 0.55,
+  },
+  card: {
+    paddingTop: 50,
+    flex: 0.45,
     backgroundColor: 'white',
   },
-  card : {
-    paddingTop : 50,
-    flex : 0.5,
-    backgroundColor: 'white',
-    
-  }
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "lightgrey",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
 });
 
-export default FieldWorkerScreen;
+export default DoctorScreen;
